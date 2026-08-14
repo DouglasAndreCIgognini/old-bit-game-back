a5639d0151eb1b5be3b1624c59b2ad8aa23d7237
DouglasAndreCigognini <douglas.andrecgn@gmail.com>
2026-08-14 02:10:21 -0300

    fix: cors autorization

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
   await app.listen(port);
 }
